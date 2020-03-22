a0177845192d03d81eda15025413865e35735300
AlexDyabkin <alexendredyabkin@gmail.com>
2020-03-22 14:14:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let pattern = bracketsConfig.map(el => el.join(''));
+
+  for(let i = 0; i < pattern.length; i++){
+    while(str.includes(pattern[i])){
+      str = str.replace(pattern[i], '');
+      i = -1;
+    }
+  }
+  return str.length == 0 ? true : false;
 }
